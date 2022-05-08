c2a74fc02e561b33ac4171dba3ff926e7f7b0e06
danielleelara <dany.sa1@hotmail.com>
2022-05-08 16:45:43 -0300

    feat: criando Topo

--- src/telas/Home/componentes/Topo.js
+++ src/telas/Home/componentes/Topo.js
@@ -1,6 +1,34 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {StyleSheet, Text, View, Image} from 'react-native';
+import logo from '../../../assets/logo.png';
 
 export default function Topo() {
-  return <Text>Topo</Text>;
+  return (
+    <View style={estilos.topo}>
+      <Image source={logo} style={estilos.imagem} />
+      <Text style={estilos.boasVindas}>Olá Danielle</Text>
+      <Text style={estilos.legenda}>Encontre os melhores produtores</Text>
+    </View>
+  );
 }
+
+const estilos = StyleSheet.create({
+  topo: {
+    backgroundColor: '#F6F6F6',
+    padding: 16,
+  },
+  imagem: {
+    width: 70,
+    height: 28,
+  },
+  boasVindas: {
+    marginTop: 24,
+    fontSize: 26,
+    lineHeight: 42,
+    fontWeight: 'bold',
+  },
+  legenda: {
+    fontSize: 16,
+    lineHeight: 26,
+  },
+});
