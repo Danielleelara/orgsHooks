202626b0a65b1711fb5c57c301d91445a8828cb8
danielleelara <dany.sa1@hotmail.com>
2022-05-08 16:27:53 -0300

    feat: inclusão imagens

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
 import Home from './src/telas/Home';
-import Topo from './src/telas/Home/componentes/Topo';
 
 export default function App() {
   return (
     <SafeAreaView>
       <Home />
-      <Topo />
     </SafeAreaView>
   );
 }
